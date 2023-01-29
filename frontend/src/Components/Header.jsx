6201aada729e05f39585599ca0b867489c976cb9
NicolasPerrin73 <nicobolas7338@gmail.com>
2023-01-29 17:07:27 +0100

    add header and video stream player

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import zionLogo from "../Assets/My zion'sound HD.png";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img src={zionLogo} alt="zionsound logo" id="zionLogo" />
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../Components/Header";
 
 const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <>
+      <Header />
+      <iframe src="http://82.165.49.163:8080/embed/video" title="Owncast" height="350px" width="550px" referrerpolicy="origin" scrolling="no" allowfullscreen></iframe>
+    </>
   );
 };
 
