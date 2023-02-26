9870592cd2b6af2c63927b4f7b540b7f3b8b2dbf
NicolasPerrin73 <nicobolas7338@gmail.com>
2023-02-26 19:25:02 +0100

    video server on zionsound server ok

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const https = require("https");
 const path = require("path");
 const fs = require("fs");
+const WebSocket = require("ws");
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -53,3 +54,128 @@ server.listen(port);
 //const sslServer = https.createServer({ key: fs.readFileSync(keyPath), cert: fs.readFileSync(certPath) }, app);
 
 //sslServer.listen(4443, () => console.log("Ssl server listening 4443"));
+
+const NodeMediaServer = require("node-media-server");
+const ffmpeg = require("ffmpeg");
+
+const config = {
+  logType: 3,
+  logFile: "./media",
+  rtmp: {
+    port: 8080,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+
+    ssl: {
+      port: 8082,
+      key: "/etc/letsencrypt/archive/zionsound.fr/privkey1.pem",
+      cert: "/etc/letsencrypt/archive/zionsound.fr/cert1.pem",
+    },
+  },
+  http: {
+    port: 8081,
+    mediaroot: "./media",
+    webroot: "./www",
+    allow_origin: "*",
+    api: true,
+  },
+  https: {
+    port: 8443,
+    key: "/etc/letsencrypt/archive/zionsound.fr/privkey1.pem",
+    cert: "/etc/letsencrypt/archive/zionsound.fr/cert1.pem",
+  },
+  /*auth: {
+    api: true,
+    api_user: "admin",
+    api_pass: "admin",
+    play: false,
+    publish: false,
+    secret: "nodemedia2017privatekey",
+  },*/
+  trans: {
+    ffmpeg: "/usr/bin/ffmpeg",
+    tasks: [
+      {
+        app: "live",
+        vc: "copy",
+        ac: "copy",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+      },
+    ],
+  },
+  fission: {
+    ffmpeg: "/usr/bin/ffmpeg",
+    tasks: [
+      {
+        rule: "live/*",
+        model: [
+          {
+            ab: "128k",
+            vb: "1500k",
+            vs: "1280x720",
+            vf: "30",
+          },
+          {
+            ab: "96k",
+            vb: "1000k",
+            vs: "854x480",
+            vf: "30",
+          },
+          {
+            ab: "96k",
+            vb: "600k",
+            vs: "640x360",
+            vf: "30",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+let nms = new NodeMediaServer(config);
+nms.run();
+
+const wss = new WebSocket.Server({ port: 8083 });
+
+let liveStreamIsActive = false;
+
+app.get("/live-stream-status", (req, res) => {
+  res.send({ isLiveStreamActive: liveStreamIsActive });
+});
+
+// Méthode postPublish
+nms.on("postPublish", (id, StreamPath, args) => {
+  console.log("Un flux a été publié !");
+  // Le flux en direct est actif
+  liveStreamIsActive = true;
+  // Envoyer le message de statut de flux en direct aux clients connectés
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ isLiveStreamActive: liveStreamIsActive }));
+    }
+  });
+});
+
+// Méthode donePublish
+nms.on("donePublish", (id, StreamPath, args) => {
+  console.log("Le flux en direct a été arrêté !");
+  // Le flux en direct est inactif
+  liveStreamIsActive = false;
+  // Envoyer le message de statut de flux en direct aux clients connectés
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ isLiveStreamActive: liveStreamIsActive }));
+    }
+  });
+});
+
+// Gérer les connexions WebSocket entrantes
+wss.on("connection", (ws) => {
+  console.log("Client connecté !");
+  // Envoyer le message de statut de flux en direct au client nouvellement connecté
+  ws.send(JSON.stringify({ isLiveStreamActive: liveStreamIsActive }));
+});
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,36 @@
-import React from "react";
 import Header from "../Components/Header";
+import ReactPlayer from "react-player/file";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isLiveStreamActive, setIsLiveStreamActive] = useState(false);
+  const [selectedQuality, setSelectedQuality] = useState("low");
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://www.zionsound.fr:8083");
+    ws.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      setIsLiveStreamActive(data.isLiveStreamActive);
+    });
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
     <>
       <Header />
-      <iframe src="http://82.165.49.163:8080/embed/video" title="Owncast" height="350px" width="550px" referrerpolicy="origin" scrolling="no" allowfullscreen></iframe>
+      {isLiveStreamActive === true ? (
+        selectedQuality === "low" ? (
+          <ReactPlayer url="http://www.zionsound.fr:8081/live/test_stream_360.flv" playing={true} controls={true} muted={true} />
+        ) : (
+          <ReactPlayer url="http://www.zionsound.fr:8081/live/test_stream.flv" playing={true} controls={true} muted={true} />
+        )
+      ) : (
+        ""
+      )}
+      <button onClick={() => setSelectedQuality("low")}>Qualité faible</button>
+      <button onClick={() => setSelectedQuality("high")}>Qualité élevée</button>
     </>
   );
 };
